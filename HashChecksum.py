bcd0f15ae43dfd9fc923bd8d7a0d8014f92ccc19
Vunkyo <remonhoogendijk@cs.com>
2018-03-20 16:59:04 +0100

    removed MD5 and SHA1 from Hashes for faster program

--- HashChecksum.py
+++ HashChecksum.py
@@ -10,25 +10,17 @@ def hashfile(inputfile):
     with open(inputfile, 'rb') as f:
         data = f.read(read_size)
         while data:
-            checksummd5.update(data)
-            checksumsha1.update(data)
             checksumsha256.update(data)
             data = f.read(read_size)
-    checksummd5 = checksummd5.hexdigest()
-    checksumsha1 = checksumsha1.hexdigest()
     checksumsha256 = checksumsha256.hexdigest()
 
 # shows de hashes in the terminal
     print("File Name: %s" % inputfile)
-    print("MD5: %r" % checksummd5)
-    print("SHA1: %r" % checksumsha1)
     print("SHA256: %r" % checksumsha256)
     print("")
 
     with open("ChecksumLog.txt", "a") as myfile:  # This writes the hashes to a file
         myfile.write("File Name: %s" % inputfile + "\n")
-        myfile.write("MD5: %r" % checksummd5 + "\n")
-        myfile.write("SHA1: %r" % checksumsha1 + "\n")
         myfile.write("SHA256: %r" % checksumsha256 + "\n")
         myfile.write("\n")
 
