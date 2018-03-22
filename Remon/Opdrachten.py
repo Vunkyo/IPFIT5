0522dd4fa7bb0477cb051d8451a667431c3e0b76
Vunkyo <remonhoogendijk@cs.com>
2018-03-23 00:58:53 +0100

    Added mbox

--- Remon/Opdrachten.py
+++ Remon/Opdrachten.py
@@ -4,33 +4,22 @@ import extract_file_type
 import os
 import readMsgFiles
 
+
 __authors__ = ["Chapin Bryce", "Preston Miller"]
 __date__ = 20170815
 __description__ = "Utility to parse text and attachments from EML files"
 
 
 def main(image, image_type, part_type):
-    loop = 1
-
-    while loop <= 5:
-        if loop == 1:
-            extract_file_type.main(image, image_type, "eml", "../Extracted", part_type)
-            loop = loop + 1
-        elif loop == 2:
-            extract_file_type.main(image, image_type, "msg", "../Extracted", part_type)
-            loop = loop + 1
-        elif loop == 3:
-            extract_file_type.main(image, image_type, "mbox", "../Extracted", part_type)
-            loop = loop + 1
-        elif loop == 4:
-            extract_file_type.main(image, image_type, "pst", "../Extracted", part_type)
-            loop = loop + 1
-        elif loop == 5:
-            extract_file_type.main(image, image_type, "ost", "../Extracted", part_type)
-            loop = loop + 1
+    extract_file_type.main(image, image_type, "eml", "../Extracted", part_type)
+    extract_file_type.main(image, image_type, "msg", "../Extracted", part_type)
+    extract_file_type.main(image, image_type, "mbox", "../Extracted", part_type)
+    extract_file_type.main(image, image_type, "ost", "../Extracted", part_type)
 
 
+# Removes duplicate addresses from file
 def removeduplicates(inputfile, outputfile):
+    os.remove(outputfile)
     lines_seen = set()  # holds lines already seen
     outfile = open(outputfile, "a")
     for line in open(inputfile, "r"):
@@ -41,15 +30,17 @@ def removeduplicates(inputfile, outputfile):
     os.remove(inputfile)
 
 
+# Locates email addresses in text files
 def find_aders(fileloc):
     in_file = open(fileloc, "rt")
     for line in in_file:
-        match = re.search(r'[\w\.-]+@[\w\.-]+', line)
+        match = re.search(r'[\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-zA-Z]{1,4}', line)
         if match is not None:
             with open("email_adres.txt", "a") as myfile:
                 myfile.write(match.group(0) + "\n")
 
 
+# reads .msg files and then searches for email addresses
 def read_msg_files():
     for root, dirs, files in os.walk("..\Extracted"):
         for filename in files:
@@ -59,12 +50,20 @@ def read_msg_files():
                 readMsgFiles.main(fileloc, "..\Extracted")
 
                 find_aders("msgmail.txt")
-    removeduplicates("email_adres.txt", "email_adres_non_dup.txt")
 
 
-# looped door een folder en print alle files met jpg extensies.
-# moet worden aangepast naar mail extensies
-# kan de mail parser uitvoeren zodra dat werkt
+# reads .mbox files and then searches for email addresses
+def read_mbox_files():
+    for root, dirs, files in os.walk("..\Extracted"):
+        for filename in files:
+            if filename.endswith(".mbox"):
+                fileloc = os.path.join(root, filename)
+                print(fileloc)
+
+                find_aders(fileloc)
+
+
+# reads .eml files and then searches for email addresses
 def read_eml_file():
     for root, dirs, files in os.walk("..\Extracted"):
         for filename in files:
@@ -73,9 +72,12 @@ def read_eml_file():
                 print(fileloc)
 
                 find_aders(fileloc)
-    removeduplicates("email_adres.txt", "email_adres_non_dup.txt")
 
 
 read_eml_file()
 read_msg_files()
+read_mbox_files()
+removeduplicates("email_adres.txt", "email_adres_non_dup.txt")
+
+
 
