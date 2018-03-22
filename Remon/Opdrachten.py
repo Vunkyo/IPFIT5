8132b515b92b99b2bbc07085f9970d4ca6d74a5e
Vunkyo <remonhoogendijk@cs.com>
2018-03-23 00:02:45 +0100

    Added .eml and .msg readers for listing addresses

--- Remon/Opdrachten.py
+++ Remon/Opdrachten.py
@@ -1,8 +1,8 @@
 from __future__ import print_function
-from argparse import ArgumentParser, FileType
+import re
 import extract_file_type
-import EML
 import os
+import readMsgFiles
 
 __authors__ = ["Chapin Bryce", "Preston Miller"]
 __date__ = 20170815
@@ -30,18 +30,52 @@ def main(image, image_type, part_type):
             loop = loop + 1
 
 
+def removeduplicates(inputfile, outputfile):
+    lines_seen = set()  # holds lines already seen
+    outfile = open(outputfile, "a")
+    for line in open(inputfile, "r"):
+        if line not in lines_seen:  # not a duplicate
+            outfile.write(line)
+            lines_seen.add(line)
+    outfile.close()
+    os.remove(inputfile)
+
+
+def find_aders(fileloc):
+    in_file = open(fileloc, "rt")
+    for line in in_file:
+        match = re.search(r'[\w\.-]+@[\w\.-]+', line)
+        if match is not None:
+            with open("email_adres.txt", "a") as myfile:
+                myfile.write(match.group(0) + "\n")
+
+
+def read_msg_files():
+    for root, dirs, files in os.walk("..\Extracted"):
+        for filename in files:
+            if filename.endswith(".msg"):
+                fileloc = os.path.join(root, filename)
+                print(fileloc)
+                readMsgFiles.main(fileloc, "..\Extracted")
+
+                find_aders("msgmail.txt")
+    removeduplicates("email_adres.txt", "email_adres_non_dup.txt")
+
+
 # looped door een folder en print alle files met jpg extensies.
 # moet worden aangepast naar mail extensies
 # kan de mail parser uitvoeren zodra dat werkt
-def recursefolder():
-    for root, dirs, files in os.walk("\Extracted"):
+def read_eml_file():
+    for root, dirs, files in os.walk("..\Extracted"):
         for filename in files:
-            if filename.endswith(".jpg"):
+            if filename.endswith(".eml"):
                 fileloc = os.path.join(root, filename)
                 print(fileloc)
 
+                find_aders(fileloc)
+    removeduplicates("email_adres.txt", "email_adres_non_dup.txt")
 
-EML.main(EML.args.EML_FILE)
 
-# recursefolder()
+read_eml_file()
+read_msg_files()
 
