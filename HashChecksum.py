e10acf0aec8ac890f47af816c11e14b84bba2b1b
Vunkyo <remonhoogendijk@cs.com>
2018-03-30 18:23:15 +0200

    Removed SHA1 and MD5

--- HashChecksum.py
+++ HashChecksum.py
@@ -3,26 +3,27 @@ import hashlib
 
 def hashfile(inputfile):
     read_size = 1024  # You can make this bigger
-    checksummd5 = hashlib.md5()
-    checksumsha1 = hashlib.sha1()
     checksumsha256 = hashlib.sha256()
 
-    with open(inputfile, 'rb') as f:
-        data = f.read(read_size)
-        while data:
-            checksumsha256.update(data)
+    try:
+        with open(inputfile, 'rb') as f:
             data = f.read(read_size)
-    checksumsha256 = checksumsha256.hexdigest()
+            while data:
+                checksumsha256.update(data)
+                data = f.read(read_size)
+        checksumsha256 = checksumsha256.hexdigest()
 
-# shows de hashes in the terminal
-    print("File Name: %s" % inputfile)
-    print("SHA256: %r" % checksumsha256)
-    print("")
+        # shows de hashes in the terminal
+        print("File Name: %s" % inputfile)
+        print("SHA256: %r" % checksumsha256)
+        print("")
 
-    with open("ChecksumLog.txt", "a") as myfile:  # This writes the hashes to a file
-        myfile.write("File Name: %s" % inputfile + "\n")
-        myfile.write("SHA256: %r" % checksumsha256 + "\n")
-        myfile.write("\n")
+        with open("ChecksumLog.txt", "a") as myfile:  # This writes the hashes to a file
+            myfile.write("File Name: %s" % inputfile + "\n")
+            myfile.write("SHA256: %r" % checksumsha256 + "\n")
+            myfile.write("\n")
+    except IOError:
+        print("There is no such file")
 
 
 def clearfile():  # Clear the log file
